951541c82de3730a468a5c3be8e1982580139092
zhangype <1141404898@qq.com>
2017-02-14 09:37:12 +0800

    Add files via upload

---
+++ src/chapter5/interceptor/StopWatchHandlerInterceptor.java
@@ -0,0 +1,32 @@
+package chapter5.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.NamedThreadLocal;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class StopWatchHandlerInterceptor extends HandlerInterceptorAdapter {
+	private NamedThreadLocal<Long> startTimeThreadLocal = new NamedThreadLocal<Long>("StopWatch-StartTime");
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		long beginTime = System.currentTimeMillis();// 1、开始时间
+		startTimeThreadLocal.set(beginTime);		// 线程绑定变量（该数据只有当前请求的线程可见）
+		return true;
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		long endTime = System.currentTimeMillis();	// 2、结束时间
+		long beginTime = startTimeThreadLocal.get();// 得到线程绑定的局部变量（开始时间）
+		long consumeTime = endTime - beginTime;		// 3、消耗的时间
+		if (consumeTime > 500) {// 此处认为处理时间超过500毫秒的请求为慢请求
+			// TODO 记录到日志文件
+			System.out.println(String.format("%s consume %d millis", request.getRequestURI(), consumeTime));
+		}
+		
+		// 使用完ThreadLocal进行清理，解决：[org.springframework.core.NamedThreadLocal] 导致的 memory leak
+		startTimeThreadLocal.remove();
+	}
+}

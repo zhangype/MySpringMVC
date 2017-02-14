260cb1579be2032f42938085e4e7943fe052a30c
zhangype <1141404898@qq.com>
2017-02-14 09:31:54 +0800

    Add files via upload

---
+++ src/chapter4/model/PhoneNumberModel.java
@@ -0,0 +1,20 @@
+package chapter4.model;
+
+public class PhoneNumberModel {
+	
+    private String areaCode;	//ÇøºÅ
+    private String phoneNumber;	//µç»°ºÅÂë
+    
+	public String getAreaCode() {
+		return areaCode;
+	}
+	public void setAreaCode(String areaCode) {
+		this.areaCode = areaCode;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}

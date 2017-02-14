f2b5bc317e11ea081b5706c2b296e040998c8637
zhangype <1141404898@qq.com>
2017-02-14 09:33:27 +0800

    Create TestController.java

---
+++ src/chapter5/controller/TestController.java
@@ -0,0 +1,17 @@
+package chapter5.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class TestController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("===========TestController===========");  
+        return new ModelAndView("test");  
+	}
+
+}
